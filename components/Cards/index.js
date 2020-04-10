0ed263a176a617a12a8de94b5e1195695101909a
Patrick Harl <therandomwoodworker@gmail.com>
2020-04-10 09:36:15 -0700

    completed sprint challenge

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,89 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+function createArticleCard(headline, imgSrc, author){
+
+    const cardContainer = document.createElement('div')
+    const cardHeadline = document.createElement('div')
+    const cardAuthor = document.createElement('div')
+    const cardImageContainer = document.createElement('div')
+    const cardImage = document.createElement('img')
+    const cardAuthorName = document.createElement('span')
+
+    cardContainer.appendChild(cardHeadline)
+    cardContainer.appendChild(cardAuthor)
+
+    cardAuthor.appendChild(cardImageContainer)
+    cardImageContainer.appendChild(cardImage)
+    cardAuthor.appendChild(cardAuthorName)
+
+    cardContainer.classList.add('card')
+    cardHeadline.classList.add('headline')
+    cardAuthor.classList.add('author')
+    cardImageContainer.classList.add('img-container')
+    
+    cardHeadline.textContent = headline
+    cardImage.src = imgSrc
+    cardAuthorName.textContent = `By ${author}`
+
+    return cardContainer
+
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+
+        const jsArray = response.data.articles.javascript
+        const bsArray = response.data.articles.bootstrap
+        const techArray = response.data.articles.technology
+        const jqueryArray = response.data.articles.jquery
+        const nodeArray = response.data.articles.node
+
+        jsArray.forEach(article => {
+
+            const newArticleCard = createArticleCard(article.headline, article.authorPhoto, article.authorName)
+            const articleParent = document.querySelector('div.cards-container')
+            articleParent.appendChild(newArticleCard)
+
+        })
+
+        bsArray.forEach(article => {
+
+            const newArticleCard = createArticleCard(article.headline, article.authorPhoto, article.authorName)
+            const articleParent = document.querySelector('div.cards-container')
+            articleParent.appendChild(newArticleCard)
+            
+        })
+
+        techArray.forEach(article => {
+
+            const newArticleCard = createArticleCard(article.headline, article.authorPhoto, article.authorName)
+            const articleParent = document.querySelector('div.cards-container')
+            articleParent.appendChild(newArticleCard)
+            
+        })
+
+        jqueryArray.forEach(article => {
+
+            const newArticleCard = createArticleCard(article.headline, article.authorPhoto, article.authorName)
+            const articleParent = document.querySelector('div.cards-container')
+            articleParent.appendChild(newArticleCard)
+            
+        })
+
+        nodeArray.forEach(article => {
+
+            const newArticleCard = createArticleCard(article.headline, article.authorPhoto, article.authorName)
+            const articleParent = document.querySelector('div.cards-container')
+            articleParent.appendChild(newArticleCard)
+            
+        })
+
+    })
+    .catch(err => {
+
+        console.log(err)
+
+
+    })
